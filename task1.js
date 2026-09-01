5f1bfeaa09442888e49f33d1288d33171b27a1fd
WarpChronicle <Varyanik98@gmail.com>
2026-09-01 15:47:11 +0300

    final_commit

--- task1.js
+++ task1.js
@@ -4,7 +4,7 @@ function Account(iban, owner, balance) {
   this.balance = balance;
   
   this.deposit = function(amount) {
-    if (amount > 0) {
+    if (amount > 0 && typeof amount === 'number') {
       this.balance += amount;
     } else {
       console.log(`Недопустимое значение для ввода: ${amount}`);
@@ -12,7 +12,7 @@ function Account(iban, owner, balance) {
   };
 
   this.withdraw = function(amount) {
-    if (this.balance >= amount) {
+    if (amount > 0 && typeof amount === 'number' && this.balance >= amount) {
         this.balance -= amount;
     } else {
       console.log(`Недостаточно средств на счете ${this.iban}`);
@@ -20,7 +20,7 @@ function Account(iban, owner, balance) {
   };
 
   this.getBalance = function() {
-    return (`Ваш баланс: ${this.balance.toFixed(2)}`);
+    return (`Ваш баланс: ${this.balance.toFixed(2)}$`);
   };
 }
 
@@ -28,7 +28,7 @@ const acc1 = new Account("ISR78234327", "John", 1000);
 const acc2 = new Account("GE372523242", "Rose", 500);
 
 acc1.deposit(300);       
-acc2.withdraw(700);
+acc2.withdraw(700); // списываю больше чем есть чтобы проверить
 
 const accounts = [acc1, acc2];
 console.log(accounts);
@@ -38,7 +38,7 @@ console.log('-----------------------');
 // b
 
 function transfer(from, to, amount) {
-  if (from.balance >= amount) {
+  if (amount > 0 && typeof amount === 'number' && from.balance >= amount) {
     from.withdraw(amount);
     to.deposit(amount);
   } else {
@@ -62,18 +62,18 @@ function transfer(from, to, amount) {
     amount: amount
   };
 
-  if (from.balance >= amount) {
+  if (amount > 0 && typeof amount === 'number' && from.balance >= amount) {
     from.withdraw(amount);
     to.deposit(amount);
 
     transaction.transInfo = function() {
-      console.log(`Успешный перевод: ${this.amount} с ${this.acc1.iban} на ${this.acc2.iban}`);
+      console.log(`Успешный перевод: на сумму ${this.amount}$ с ${this.acc1.iban} на ${this.acc2.iban}`);
     };
   } else {
     transaction.error = "Недостаточно средств на счете списания";
     
     transaction.transInfo = function() {
-      console.log(`Ошибка транзакции (${this.error}): не удалось перевести ${this.amount} с ${this.acc1.iban}`);
+      console.log(`Ошибка транзакции (${this.error}): не удалось перевести ${this.amount}$ с ${this.acc1.iban}`);
     };
   }
 
